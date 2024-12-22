a613128119a731b9d80a27e9d8498272e5186b19
Orisama03 <112028499+Orisama03@users.noreply.github.com>
2024-12-22 16:42:16 -0600

    Añadir interactividad a la tarjeta de Navidad y mejorar el diseño de la interfaz

--- js/nieve.js
+++ js/nieve.js
@@ -59,3 +59,10 @@ setInterval(() => {
     createSnowflake();
   }
 }, 2000);
+
+const card = document.querySelector('.card-n');
+
+card.addEventListener('click', function() {
+    // al hacer click, agrega clase css open-n al card-n
+    card.classList.toggle('open-n')
+});
